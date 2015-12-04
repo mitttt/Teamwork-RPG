eda8c6f57da0e688e1dd8def2ab19f1d3fa86a70
Natureknight <vallez8244@gmail.com>
2015-12-04 11:05:15 +0200

    Initialization of the project

---
+++ TeamworkRPG/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* This is the initial state of the project of team Dezda */
+
+namespace TeamworkRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
